dadaf1350bb6e898fcb20887acf722fc5f3cd803
James Peilow <james@jamespeilow.com>
2021-11-09 21:13:13 +0000

    feature: added clip path animation for hover elements

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,10 +5,13 @@
   >
     <div
       v-for="(item, index) in items"
+      :key="item.title"
       class="column"
       :style="{
         backgroundImage: `url(${item.image}`
       }"
+      @mouseover="item.showOverlay = true"
+      @mouseleave="item.showOverlay = false"
     >
       <p class="column__number">
         {{ formatNumber(index + 1) }}
@@ -16,14 +19,18 @@
       <div class="column__content">
         {{ item.title }}
       </div>
-      <div
-        class="column__hover-content"
-        :style="{ backgroundColor: item.color }"
-      >
-        <p class="column__hover-text">
-          {{ item.title }}
-        </p>
-      </div>
+
+      <transition>
+        <div
+          v-if="item.showOverlay"
+          class="column__hover-content"
+          :style="{ backgroundColor: item.color }"
+        >
+          <p class="column__hover-text">
+            {{ item.title }}
+          </p>
+        </div>
+      </transition>
     </div>
   </div>
 </template>
@@ -36,26 +43,31 @@ export default {
           color: '#5A3020',
           image: 'https://images.igdb.com/igdb/image/upload/t_original/ar4sz.jpg',
           title: 'The Legend of Zelda: Breath of the Wild',
+          showOverlay: false,
         },
         {
           color: '#4B4319',
           image: 'https://images.igdb.com/igdb/image/upload/t_original/hj3spi7odlvp4pfvnwtv.jpg',
           title: 'Super Mario Odyssey',
+          showOverlay: false,
         },
         {
           color: '#542662',
           image: 'https://images.igdb.com/igdb/image/upload/t_original/ar7u5.jpg',
           title: 'Celeste',
+          showOverlay: false,
         },
         {
           color: '#003345',
           image: 'https://images.igdb.com/igdb/image/upload/t_original/m18xmrbbbzlmh8bru5lm.jpg',
           title: 'Hollow Knight',
+          showOverlay: false,
         },
         {
           color: '#750525',
           image: 'https://images.igdb.com/igdb/image/upload/t_original/fd9fak2dziwkrewahczb.jpg',
           title: 'Mario Kart 8 Deluxe',
+          showOverlay: false,
         }
       ]
     }
@@ -126,15 +138,28 @@ export default {
 
   &__hover-content {
     background: red;
+    clip-path: polygon(0 0, 100% 0, 100% 100%, 0 100%);
     display: flex;
     justify-content: center;
     height: 100%;
     left: 0;
     position: absolute;
     top: 0;
-    transform: translateY(-100%);
     width: 100%;
     z-index: 2;
+
+    &.v-enter-active {
+      transition: clip-path 0.35s ease-in-out;
+    }
+
+    &.v-leave-active {
+      transition: clip-path 0.25s ease-in-out;
+    }
+
+    &.v-enter-from,
+    &.v-leave-to {
+      clip-path: polygon(0 100%, 100% 100%, 100% 100%, 0 100%);
+    }
   }
 
   &__hover-text {
@@ -146,12 +171,6 @@ export default {
     text-transform: uppercase;
     transform: rotate(180deg);
     writing-mode: vertical-rl;
-  }
-
-  &:hover {
-    .column__hover-content {
-      transform: translateY(0);
-    }
   }
 }
 </style>
