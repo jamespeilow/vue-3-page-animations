3d683875e579b6fe47df4c9f4ca5fafcf63b366c
James Peilow <james@jamespeilow.com>
2021-11-09 21:23:09 +0000

    feature: set up vue router with new game route

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+/**
+ * Router setup
+ */
+const routes = [
+  { path: '/', component: () => import('./components/Home.vue') },
+  { path: '/game', component: () => import('./components/Game.vue') },
+]
+
+const router = new createRouter({
+  history: createWebHashHistory(),
+  routes,
+})
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
