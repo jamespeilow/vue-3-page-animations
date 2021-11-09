022b4d7f28a9727153b0095f0bdc611a3844ad5c
James Peilow <james@jamespeilow.com>
2021-11-09 21:54:20 +0000

    feature: set up initial layout and styling for game page

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,5 +1,38 @@
 <template>
-  <h1>Game</h1>
+  <main class="content">
+    <div class="content__wrapper">
+      <h1 class="content__title">Title</h1>
+      <p class="content__meta">
+        Release date: dd/mm/yyyy
+      </p>
+
+      <div class="content__grid">
+        <div class="content__video">
+
+        </div>
+        
+        <div class="content__image content__image--1">
+
+        </div>
+
+        <div class="content__image content__image--2">
+
+        </div>
+
+        <div class="content__image content__image--3">
+
+        </div>
+
+        <p class="content__description">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod, nisi vel consectetur egestas, nisl nunc
+        </p>
+      </div>
+    </div>
+
+    <div class="content__carousel">
+
+    </div>
+  </main>
 </template>
 
 <script>
@@ -8,6 +41,50 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.content {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  min-height: 100vh;
 
+  &__carousel {
+    background-color: gold;
+  }
+
+  &__wrapper {
+    margin: 4rem 2rem 1rem;
+  }
+
+  &__title {
+    font-size: 2.5rem;
+    margin: 0 0 0.25rem;
+    text-transform: uppercase;
+  }
+
+  &__meta {
+    margin: 0 0 2rem;
+    text-transform: uppercase;
+  }
+
+  &__grid {
+    display: grid;
+    grid-auto-flow: dense;
+    grid-template-columns: 3fr 2fr;
+    gap: 2rem 1rem;
+  }
+
+  &__video {
+    background-color: #000;
+    grid-row: 1/3;
+    min-height: 200px;
+  }
+
+  &__image {
+    background-color: #666;
+    height: 110px;
+    display: block;
+    padding: 1rem;
+    grid-column: 2/3;
+  }
+}
 </style>
